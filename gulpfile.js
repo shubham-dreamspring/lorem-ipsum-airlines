36ba01bed9f70bee8f5d9deed1c6cc48fac814a7
shubham-dreamspring <sjain@dreamspring.org>
2023-02-21 19:15:42 +0530

    Compress CSS file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const purgecss = require("gulp-purgecss");
+const rename = require("gulp-rename");
 
 function buildStyles() {
   return src("styles/scss/**/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
     .pipe(purgecss({ content: ["index.html", "pages/*.html"] }))
+    .pipe(
+      rename(function (file) {
+        file.basename = file.basename + ".min";
+      })
+    )
     .pipe(dest("styles/css"));
 }
 
